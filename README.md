42df5f3eded107566188b1dcf816125725c8e2fa
bondoki <bondoki@gmx.de>
2021-04-18 22:09:30 +0200

    * adding licence and acknowledgement

---
+++ README.md
@@ -0,0 +1,41 @@
+# Match3-PGE 
+Simple clone of the common match3 genre using the [olc::PixelGameEngine](https://github.com/OneLoneCoder/olcPixelGameEngine) by [Javidx9](https://github.com/OneLoneCoder).
+It's just for fun and educational purpose. Feel free to modify and use it :)
+If you want to use the assets, feel free to use it. Acknowledgement is highly appreciated. 
+
+![Match3-PGE_Screenshot_20210418](https://user-images.githubusercontent.com/16195016/115147815-2a7a6880-a05d-11eb-9505-4ae5e90ec12c.png)
+ 
+## Installation
+Just do for standard compilation on Linux (tested with gcc 8.2.1):
+
+````sh
+    # generates the application
+    g++ -o Jewel mainJewels.cpp -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17
+````
+
+## Usage
+Simply use the mouse to select a gem and another to switch creating rows or coloumns.
+
+Special gems:
+- Bomb: 4 gems in consecutive order row/column
+- Rainbow: 5 gems in consecutive order row/column
+
+Effect:
+- Bomb: destroy 3x3 vicinity of bomb
+- Rainbow: destroy all gems of same color
+
+Random Drop:
+- Bomb: 1/64
+- Rainbow: 1/4096
+
+ESC : quit
+
+
+## References and License
+The program is licensed und [OLC-3](https://github.com/OneLoneCoder/olcPixelGameEngine/blob/master/LICENCE.md) and acknowledge gratefully the following libraries and refer to the appropriate licenses
+* [olcPixelGameEngine](https://github.com/OneLoneCoder/olcPixelGameEngine) by [Javidx9](https://github.com/OneLoneCoder)
+* [olcPGEX_AnimatedSprite](https://github.com/matt-hayward/olcPGEX_AnimatedSprite) by [matt-hayward](https://github.com/matt-hayward)
+
+## Acknowledgment
+Heavily inspired by the Match3 example in the [50K Live code party](https://www.youtube.com/watch?v=7y8Zg87rtjs) of [Javidx9](https://github.com/OneLoneCoder) giving the idea for this.<br>
+For the original source with olcConsoleGameEngine please see: [50K Live code party source code](https://github.com/OneLoneCoder/videos/blob/master/OneLoneCoder_MatchingGems_50KSubSpecial.cpp).
--- mainJewels.cpp
+++ mainJewels.cpp
@@ -40,13 +40,37 @@
  * + Cursor sprite
  * + check for simultaneous 3 row and 3 column
  * 
- * License: This piece of code is licensed to OLC-3 according to
+ * License: This piece of code is licensed to OLC-3 according to (see below)
  * https://github.com/OneLoneCoder/olcPixelGameEngine/blob/master/LICENCE.md
  * also attributing Javidx9 for the unique and easy-to-use olc::PixelGameEngine and the underlying OLC-3 license see
  * https://github.com/OneLoneCoder/olcPixelGameEngine/wiki/Licencing
  * For more information about Javidx9 and OneLoneCoder please see https://github.com/OneLoneCoder
  * Also big thanks to Matt Hayward, 0xnicholasc, and Moros1138 for the olcPGEX_AnimatedSprite extension.
  * Please see https://github.com/matt-hayward/olcPGEX_AnimatedSprite and the underlying OLC-3 license.
+ * 
+ * License (OLC-3)
+	~~~~~~~~~~~~~~~
+	Copyright 2018 - 2021 OneLoneCoder.com
+	Redistribution and use in source and binary forms, with or without modification,
+	are permitted provided that the following conditions are met:
+	1. Redistributions or derivations of source code must retain the above copyright
+	notice, this list of conditions and the following disclaimer.
+	2. Redistributions or derivative works in binary form must reproduce the above
+	copyright notice. This list of conditions and the following	disclaimer must be
+	reproduced in the documentation and/or other materials provided with the distribution.
+	3. Neither the name of the copyright holder nor the names of its contributors may
+	be used to endorse or promote products derived from this software without specific
+	prior written permission.
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS	"AS IS" AND ANY
+	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+	OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+	SHALL THE COPYRIGHT	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+	INCIDENTAL,	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+	BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+	SUCH DAMAGE.
  */
 
 #define BOARD_X 8
